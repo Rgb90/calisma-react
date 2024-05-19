84cf5718b3527b47a0841591273402af1a003455
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-05-19 14:04:05 +0300

    iki

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@ import './App.css'
 import React from 'react';
 
 function App() {
-  const date = new Date(2024, 1, 1, 19);
+  const date = new Date(); // new Date(2024, 1, 1, 19), new Date(2024, 1, 1, 15), new Date(2024, 1, 1, 10) değiştir, hızlı gör
   const currentHour = date.getHours();
 
   let greetingMessage;
 
+  const customStyle = {
+    color: ""
+  }
+
   if (currentHour < 12) {
-    greetingMessage = "Hayırlı sabahlar"
+    greetingMessage = "Hayırlı sabahlar";
+    customStyle.color = "red";
   } else if (currentHour < 18) {
-    greetingMessage = "Hayırlı günler"
-  } else { greetingMessage = "Hayırlı geceler" }
+    greetingMessage = "Hayırlı günler";
+    customStyle.color = "green"; // Öğlen sonrası yeşil renk
+  } else {
+    greetingMessage = "Hayırlı geceler";
+    customStyle.color = "blue"; // Akşam mavi renk (örnek)
+  }
 
   return (
     <>
-      <h1>{greetingMessage}</h1>
+      <h1 className='heading' style={customStyle}>{greetingMessage}</h1>
     </>
   );
 }
